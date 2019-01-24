20717234a35ed14727f9fa99ea284db42c4ebfcd
Christian Cardona <christianc1078@live.saisd.net>
2019-01-24 17:03:06 -0600

    Thu Jan 24 17:03:06 CST 2019

--- Cardona_8_java0.java
+++ Cardona_8_java0.java
@@ -1,1 +1,53 @@
-/home/christianc1078/NetBeansProjects/Cardona_8_java0/src/cardona_8_java0/Cardona_8_java0.java
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardona_8_java0;
+
+/**
+ *
+ * @author christianc1078
+ */
+public class Cardona_8_java0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Animal tiger = new Animal ("tim");
+      Animal lion;
+        lion = new Animal ("kayla"+"reed");
+        tiger.GoMeow();
+        lion.GoPoop();
+      
+    }
+   
+}
+class Animal {
+    int age;
+    String lastname;
+    String firstname;
+    boolean isAnimal;
+    public Animal()  {        
+    }
+
+    public Animal(String fn){
+        this.firstname=fn;
+        
+    }
+    public Animal(String fn, String ln, int a){
+        this.firstname=fn;
+        this.lastname=ln;
+        this.age = a;
+    }
+        public void GoMeow(){
+            System.out.println("meow");
+                
+    }
+
+        public void GoPoop(){
+           System.out.println("lion takes dump");
+    }
+}
+
